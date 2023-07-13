279fdb0c4ccf5309e79d11e88f7d833e43cab291
veyselDursun <veysdursun@yahoo.com>
2023-07-13 10:47:25 +0200

    day1

--- src/test/java/stepdefinitions/AmazonStepdefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepdefinitions.java
@@ -1,7 +1,9 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 import pages.AmazonPage;
 import utilities.ConfigReader;
@@ -25,13 +27,39 @@ public class AmazonStepdefinitions {
 
     @Then("arama sonuclarinin Nutella icerdigini test eder")
     public void arama_sonuclarinin_nutella_icerdigini_test_eder() {
-        String expected = "Nutella";
-        String actualSonuc =
+        String expectedIcerik = "Nutella";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
 
     }
 
     @Then("sayfayi kapatir")
     public void sayfayi_kapatir() {
 
+    }
+
+    @Then("arama kutusuna Samsung yazip, ENTER tusuna basar")
+    public void aramaKutusunaSamsungYazipENTERTusunaBasar() {
+        amazonPage.aramaKutusu.sendKeys("Samsung" + Keys.ENTER);
+    }
+
+    @And("arama sonuclarinin Samsung icerdigini test eder")
+    public void aramaSonuclarininSamsungIcerdiginiTestEder() {
+        String expectedIcerik = "Samsung";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
+    }
+
+    @Then("arama kutusuna Java yazip, ENTER tusuna basar")
+    public void aramaKutusunaJavaYazipENTERTusunaBasar() {
+        amazonPage.aramaKutusu.sendKeys("Java" + Keys.ENTER);
+
+    }
+
+    @And("arama sonuclarinin Java icerdigini test eder")
+    public void aramaSonuclarininJavaIcerdiginiTestEder() {
+        String expectedIcerik = "Java";
+        String actualSonucYazisi = amazonPage.sonucYaziElementi.getText();
+        Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
     }
 }
--- target/test-classes/US1001_NutellaAramaTesti.feature
+++ target/test-classes/US1001_NutellaAramaTesti.feature
@@ -1,8 +1,22 @@
 Feature: US1001 Nutella arama testi
 
   Scenario: TC01 Kullanici amazonda Nutella arayabilmeli
-
     Given kullanici amazon ana sayfaya gider
     Then arama kutusuna Nutella yazip, ENTER tusuna basar
     And arama sonuclarinin Nutella icerdigini test eder
     And sayfayi kapatir
+
+
+  Scenario: TC02 Kullanici amazonda Samsung aratabilmeli
+    Given kullanici amazon ana sayfaya gider
+    Then arama kutusuna Samsung yazip, ENTER tusuna basar
+    And arama sonuclarinin Samsung icerdigini test eder
+    And sayfayi kapatir
+
+
+  Scenario: TC03 kullanici amazonda Java aratabilmesi
+    Given kullanici amazon ana sayfaya gider
+    Then arama kutusuna Java yazip, ENTER tusuna basar
+    And arama sonuclarinin Java icerdigini test eder
+    And sayfayi kapatir
+
